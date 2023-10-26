1162ed11dc54f24cc9ce85b2780eb562151cb655
Samuel Ajibade <samuelajibade22@gmail.com>
2023-10-26 16:42:05 +0100

    modified HomeScreen layout added custom styles

--- app/src/main/java/com/ajibsbaba/acefood/MainActivity.kt
+++ app/src/main/java/com/ajibsbaba/acefood/MainActivity.kt
@@ -8,20 +8,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -32,6 +39,8 @@ import androidx.compose.ui.unit.sp
 import com.ajibsbaba.acefood.ui.theme.AcefoodTheme
 import com.ajibsbaba.acefood.ui.theme.Typography
 import com.ajibsbaba.acefood.ui.theme.axiformaFamily
+import com.ajibsbaba.acefood.ui.theme.black50
+import com.ajibsbaba.acefood.ui.theme.gray35
 import com.ajibsbaba.acefood.ui.theme.red100
 import com.ajibsbaba.acefood.ui.theme.white100
 
@@ -50,22 +59,33 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HomeScreen() {
     Scaffold(
+        containerColor = gray35,
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = gray35),
                 title = {
                     Image(painterResource(id = R.drawable.acefood_logo), contentDescription = "Acefood Logo", modifier = Modifier.width(94.dp))
                 }
             )
         },
+        bottomBar = {}
         ) { innerPadding ->
         Column(
             modifier = Modifier
-                .padding(innerPadding) .padding(start = 16.dp, end = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(start = 16.dp, end = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
         ) {
-            UserCard()
-            ScanButton {
+            Column() {
+                UserCard()
+                ScanButton {
+                }
+                DiseaseDetailsCard()
             }
+
+
+            RecentScansCard()
             }
         }
     }
@@ -85,14 +105,22 @@ fun UserCard() {
 
 @Composable
 fun ScanButton(onClick: () -> Unit) {
-    Column(verticalArrangement = Arrangement.Center,modifier = Modifier
-        .background(red100)
+    Card(modifier = Modifier
         .fillMaxWidth()
-        .padding(start = 16.dp)
-        .height(70.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-            Icon(tint = red100, imageVector = Icons.Outlined.Search, contentDescription = "Search Icon", modifier = Modifier.padding(end = 14.dp) .background(
-                white100) .padding(4.dp))
+        .height(70.dp), colors = CardDefaults.cardColors(
+        containerColor = red100
+    )) {
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start, modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(start = 16.dp)) {
+            Icon(tint = red100, imageVector = Icons.Outlined.Search, contentDescription = "Search Icon", modifier = Modifier
+                .padding(end = 14.dp)
+                .clip(RoundedCornerShape(50.dp))
+                .background(
+                    white100
+                )
+                .padding(5.dp))
             Column {
                 Text(text = "Start a disease scan",
                     style = TextStyle(
@@ -119,13 +147,168 @@ fun ScanButton(onClick: () -> Unit) {
 }
 
 
-@Preview(showBackground = true, )
 @Composable
-fun CardPreview() {
-    AcefoodTheme {
-        UserCard()
+fun DiseaseDetailsCard() {
+    Card(modifier =
+    Modifier
+        .fillMaxWidth()
+        .height(200.dp)
+        .padding(top = 20.dp), colors = CardDefaults.cardColors(
+            containerColor = white100
+        )) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                Text(
+                    text = "Explore Types of Diseases",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = axiformaFamily),
+                        fontWeight = FontWeight.ExtraBold,
+                        color = black50,
+                    )
+            }
     }
 }
+
+@Composable
+fun RecentScansCard() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(bottom = 30.dp)) {
+        Text(
+            text = "Recent Scans",
+            modifier = Modifier.padding(bottom = 16.dp),
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontFamily = axiformaFamily,
+                fontWeight = FontWeight.ExtraBold,
+                color = black50,
+                )
+        )
+        Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                white100
+            )
+            .padding(16.dp) ) {
+            Column {
+                Text(
+                    text = "Tomato",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = black50,
+
+                        )
+                )
+                Text(
+                    text = "Late Blight",
+                    style = TextStyle(
+                        fontSize = 13.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+            }
+            Column(horizontalAlignment = Alignment.End) {
+                Text(
+                    text = "73%",
+                    style = TextStyle(
+                        fontSize = 13.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+                Text(
+                    text = "Accuracy",
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+            }
+        }
+        Row(modifier = Modifier.height(12.dp)) {
+
+        }
+        Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                white100
+            )
+            .padding(16.dp) ) {
+            Column {
+                Text(
+                    text = "Tomato",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.ExtraBold,
+                        color = black50,
+
+                        )
+                )
+                Text(
+                    text = "Late Blight",
+                    style = TextStyle(
+                        fontSize = 13.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+            }
+            Column(horizontalAlignment = Alignment.End) {
+                Text(
+                    text = "73%",
+                    style = TextStyle(
+                        fontSize = 13.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+                Text(
+                    text = "Accuracy",
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        fontFamily = axiformaFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = black50,
+
+                        )
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun PlantDiseaseList() {
+
+}
+@Preview(showBackground = true)
+@Composable
+fun RecentScansPreview() {
+    AcefoodTheme {
+        RecentScansCard()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
@@ -135,7 +318,7 @@ fun ButtonPreview() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreenPreview() {
     AcefoodTheme {
