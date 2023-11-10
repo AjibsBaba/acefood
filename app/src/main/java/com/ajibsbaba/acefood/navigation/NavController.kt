28f3f7c3899b103691679b6acee07d5d081bb646
Samuel Ajibade <samuelajibade22@gmail.com>
2023-11-10 22:19:23 +0100

    created authentication screens layout

---
+++ app/src/main/java/com/ajibsbaba/acefood/navigation/NavController.kt
@@ -0,0 +1,15 @@
+package com.ajibsbaba.acefood.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = "home" ) {
+
+    }
+}
